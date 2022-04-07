a9f12a4d16afaffa98901fe15f6bb1e2822c9b56
Willie Ruemmele <willieruemmele@gmail.com>
2022-04-07 09:52:55 -0600

    chore: more refactoring

--- src/commands/auth/logout.ts
+++ src/commands/auth/logout.ts
@@ -73,7 +73,7 @@ export default class Logout extends SfdxCommand {
   }
 
   private async shouldRunCommand(usernames: string[]): Promise<boolean> {
-    const orgsToDelete = [...usernames.join(os.EOL)];
+    const orgsToDelete = [usernames.join(os.EOL)];
     const message = messages.getMessage('logoutCommandYesNo', orgsToDelete);
     return Prompts.shouldRunCommand(this.ux, this.flags.noprompt, message);
   }
